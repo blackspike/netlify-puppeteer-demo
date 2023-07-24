066e8c8d451783950f00f85fb98289b8ac17bc22
felix corke <felix@blackspike.com>
2023-07-24 14:06:21 +0100

    working!

--- README.md
+++ README.md
@@ -2,11 +2,24 @@
 
 We had a real hassle getting puppeteer working on Netlify, but we did, and here is a demo project for our reference.
 
+package.json:
+
 ```
 "dependencies": {
   "@sparticuz/chromium": "^113.0.1",
   "puppeteer-core": "^20.1.1"
 }
+```
+netlify.toml:
+```
+[functions]
+  node_bundler = "esbuild"
+  external_node_modules = ["@sparticuz/chromium"]
+```
+
+env var
+```
+CHROME_EXECUTABLE_PATH="Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 ```
 
 by [blackspike design](https://www.blackspike.com)
---
+++ netlify/functions/puppet.js
@@ -0,0 +1,47 @@
+import chromium from '@sparticuz/chromium'
+import puppeteer from 'puppeteer-core'
+
+// put in env
+const CHROME_EXECUTABLE_PATH = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+const url = 'https://lite.cnn.com/'
+
+chromium.setHeadlessMode = true
+chromium.setGraphicsMode = false
+
+export async function handler(event, context) {
+  try {
+    const browser = await puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: CHROME_EXECUTABLE_PATH || (await chromium.executablePath('/var/task/node_modules/@sparticuz/chromium/bin')),
+    })
+
+    const page = await browser.newPage()
+
+    await page.goto(url)
+
+    await page.waitForSelector('.title')
+
+    const results = await page.$$eval('ul li', (articles) => {
+      return articles.map((link) => {
+        return {
+          title: link.querySelector('a').innerText,
+          url: link.querySelector('a').href,
+        }
+      })
+    })
+
+    await browser.close()
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(results),
+    }
+  } catch (error) {
+    console.error(error)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error }),
+    }
+  }
+}
